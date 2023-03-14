a41a51f2791140c7a30663ccec48a17196ede28a
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2023-03-14 00:35:36 +0000

    Updates

---
+++ precache-manifest.4d20f22015179d4ca67a06bb7d8f8c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99907a7666cac2104c1b",
+    "url": "/mortgage-calculator/static/css/main.f0ac1524.chunk.css"
+  },
+  {
+    "revision": "99907a7666cac2104c1b",
+    "url": "/mortgage-calculator/static/js/main.99907a76.chunk.js"
+  },
+  {
+    "revision": "7896f7e2acaf3de05c00",
+    "url": "/mortgage-calculator/static/css/1.e17fdac4.chunk.css"
+  },
+  {
+    "revision": "7896f7e2acaf3de05c00",
+    "url": "/mortgage-calculator/static/js/1.7896f7e2.chunk.js"
+  },
+  {
+    "revision": "c06bfee6c5e663c81c01",
+    "url": "/mortgage-calculator/static/js/runtime~main.c06bfee6.js"
+  },
+  {
+    "revision": "70493c9a6390130e9f1ce5b64713bfba",
+    "url": "/mortgage-calculator/index.html"
+  }
+];
